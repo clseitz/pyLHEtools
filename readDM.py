01a4b47a2db4181a083964e46a459c1b403af1aa
claudia seitz <clseitz.physics@gmail.com>
2017-04-24 17:11:39 +0200

    add tree from Deborah, change output filen name

--- readDM.py
+++ readDM.py
@@ -6,26 +6,59 @@ from LHEfile import *
 import plotTools
 from array import array
 import numpy as n
+import os
 if __name__ == '__main__':
 
     # find events in file
-    myLHEfile = LHEfile(sys.argv[1])
+    finName = ""
+    foutName = ""
+    if len(sys.argv) == 2:                                                                                                                               
+        finName = sys.argv[1]
+        foutName = os.path.basename(finName).replace('.lhe','.root')
+    elif len(sys.argv) == 3:
+        finName = sys.argv[1]
+        foutName = sys.argv[2]
+    elif len(sys.argv) < 2:
+        print "No input LHE file given"
+        exit(0)
+    
+        
+    myLHEfile = LHEfile(finName)
     myLHEfile.setMax(10000)
 
     eventsReadIn = myLHEfile.readEvents()
 
+    #define tree
     t = rt.TTree( 'events', 'tree with events from LHE file' )
     nPart =  array('i',[50])
     np = 0
-    ID = array('f',nPart[0]*[0])
-    t.Branch("nPart",nPart,"nPart/I")
-    t.Branch("ID",ID,"ID[np]/F")
-#    t.Branch("E",E,"E[nPart]/F")
-#    t.Branch("Px",Px,"Px[nPart]/F")
-#    t.Branch("Py",Py,"Py[nPart]/F")
-#    t.Branch("Pz",Pz,"Pz[nPart]/F")
 
-#    tree.Branch( 'staff', staff, '' )
+    PdgID = array('i',nPart[0]*[0])
+    mIdx = array('i',nPart[0]*[0])
+    E = array('f',nPart[0]*[0])
+    Px = array('f',nPart[0]*[0])
+    Py = array('f',nPart[0]*[0])
+    Pz = array('f',nPart[0]*[0])
+    Pt = array('f',nPart[0]*[0])
+
+    M = array('f',nPart[0]*[0])
+    Eta = array('f',nPart[0]*[0])
+    Phi = array('f',nPart[0]*[0])
+
+    t.Branch("nPart",nPart,"nPart/I")
+    t.Branch("PdgID",PdgID,"PdgID[nPart]/I")
+    t.Branch("mIdx",mIdx,"mIdx[nPart]/I")
+    t.Branch("E",E,"E[nPart]/F")
+    t.Branch("Px",Px,"Px[nPart]/F")
+    t.Branch("Py",Py,"Py[nPart]/F")
+    t.Branch("Pz",Pz,"Pz[nPart]/F")
+    t.Branch("Pt",Pt,"Pt[nPart]/F")
+    t.Branch("M",Pt,"M[nPart]/F")
+    t.Branch("Eta",Eta,"Eta[nPart]/F")
+    t.Branch("Phi",Phi,"Phi[nPart]/F")
+
+    vect = rt.TLorentzVector(0,0,0,0)
+
     for oneEvent in eventsReadIn:
 
         myLHEevent = LHEevent()
@@ -33,15 +66,30 @@ if __name__ == '__main__':
         w = myLHEevent.Weights
         nPart[0] = int(len(myLHEevent.Particles))
         np = int(len(myLHEevent.Particles))
-        print nPart
         for i,p  in enumerate(myLHEevent.Particles):
-            ID[i] = p['ID']
-        print ID
+
+            PdgID[i] = p['PdgID']
+            mIdx[i] = p['mIdx']
+            E[i] = p['E']
+            Px[i] = p['Px']
+            Py[i] = p['Py']
+            Pz[i] = p['Pz']
+            vect.SetPxPyPzE(Px[i],Py[i],Pz[i],E[i])
+            Pt[i] = vect.Pt()
+            M[i] = vect.M()
+ 
+            Eta[i] = -999
+            Phi[i] = -999
+            if vect.Pt() > 0:
+                Eta[i] = vect.Eta()
+                Phi[i] = vect.Phi()
+                
         t.Fill()
 
 
             
 
         del oneEvent, myLHEevent
-    fout =  rt.TFile(sys.argv[2],"RECREATE") 
+    print "creating output root file", foutName
+    fout =  rt.TFile(foutName,"RECREATE") 
     t.Write()
