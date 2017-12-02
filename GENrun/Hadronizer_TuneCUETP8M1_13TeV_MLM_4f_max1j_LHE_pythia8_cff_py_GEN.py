b4d77c67b7843dc8dbb9ea2edbec62f1d84f54fe
Claudia <clseitz@physics.rutgers.edu>
2017-12-02 12:07:27 +0100

    remove dilepton flag

--- GENrun/Hadronizer_TuneCUETP8M1_13TeV_MLM_4f_max1j_LHE_pythia8_cff_py_GEN.py
+++ GENrun/Hadronizer_TuneCUETP8M1_13TeV_MLM_4f_max1j_LHE_pythia8_cff_py_GEN.py
@@ -13,7 +13,6 @@ if len(sys.argv) > 5:
 	RunEvents = int(sys.argv[3])
 	JobNumber = int(sys.argv[4])
 	outfolder = sys.argv[5]
-	ttDilep = sys.argv[6]
 	start = fin.find('ttDM')
 	stop=fin.find('/Eve')
 	#fout = (os.path.split(fin)[1]).replace('.lhe','_Pythia8_'+str(JobNumber)+'_GEN.root')
